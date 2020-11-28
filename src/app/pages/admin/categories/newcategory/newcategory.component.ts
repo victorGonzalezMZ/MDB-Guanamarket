675ab25cd100e663df2d20343a24fe87667fb953
EmmanuelNOVA <emmanuel.noriegav@gmail.com>
2020-11-28 10:58:44 -0600

    Se agregan metodos para agregar nuevas categorias

--- src/app/pages/admin/categories/newcategory/newcategory.component.ts
+++ src/app/pages/admin/categories/newcategory/newcategory.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { CategoriesService } from 'src/app/services/core/categories.service';
+import Swal from 'sweetalert2'
 
 @Component({
-  selector: 'app-newcategory',
-  templateUrl: './newcategory.component.html',
-  styleUrls: ['./newcategory.component.scss']
+	selector: 'app-newcategory',
+	templateUrl: './newcategory.component.html',
+	styleUrls: ['./newcategory.component.scss']
 })
 export class NewcategoryComponent implements OnInit {
 
-  constructor() { }
+	constructor(private categorySvc: CategoriesService) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
-  registerNewCategory(form: NgForm) {
-  }
+	registerNewCategory(form: NgForm) {
+		const obj = {
+			"name": form.value.name
+		};
+
+		this.categorySvc.insertCategory(obj).subscribe(response => {
+			if(response>0){
+				Swal.fire(
+					'Bien hecho!',
+					`Tu categori con ID ${response} fue registrado correctamente!`,
+					'success'
+				)
+			}
+		}, err => {
+			console.log(err);
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: `${err}`,
+			});
+		});
+	}
 
 
 }
