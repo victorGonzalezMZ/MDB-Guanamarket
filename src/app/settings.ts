b7904cd515b2970e4d47fbe8b6e685b240235e92
EmmanuelNOVA <emmanuel.noriegav@gmail.com>
2020-12-11 16:13:43 -0600

    Cambio server

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,9 +1,12 @@
 export default {
     apinode: {
         /*urlServer: "http://localhost:3800/api/",*/
-        urlServer: "http://localhost:3000/v1/",
-        urlPhotoServer: "http://localhost:3000/v1/product/get-image-product/",
-        urlPhotoUserServer: "http://localhost:3000/v1/user/get-image-user/"
+        //urlServer: "http://localhost:3000/v1/",
+        //urlPhotoServer: "http://localhost:3000/v1/product/get-image-product/",
+        //urlPhotoUserServer: "http://localhost:3000/v1/user/get-image-user/"
+        urlServer: "https://api-guanamarket-r6tit.ondigitalocean.app/v1/",
+        urlPhotoServer: "https://api-guanamarket-r6tit.ondigitalocean.app/v1/product/get-image-product/",
+        urlPhotoUserServer: "https://api-guanamarket-r6tit.ondigitalocean.app/v1/get-image-user/"
     },
 
     apinetcore:{
