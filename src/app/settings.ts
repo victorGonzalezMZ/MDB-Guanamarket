84ac574cb723e43f43ff744c0e171d0f77d68231
unknown <victor309710gonz@gmail.com>
2021-02-17 11:20:03 -0600

    first commit

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,13 +1,9 @@
 export default {
     apinode: {
-        /*urlServer: "http://localhost:3800/api/",*/
         urlServer: "http://apinode.manenova.com/v1/",
         urlPhotoServer: "http://apinode.manenova.com/v1/product/get-image-product/",
         urlPhotoUserServer: "http://apinode.manenova.com/v1/user/get-image-user/"
-        //urlServer: "https://api-guanamarket-r6tit.ondigitalocean.app/v1/",
-        //urlPhotoServer: "https://api-guanamarket-r6tit.ondigitalocean.app/v1/product/get-image-product/",
-        //urlPhotoUserServer: "https://api-guanamarket-r6tit.ondigitalocean.app/v1/user/get-image-user/"
-    },
+     },
 
     apinetcore:{
         urlServer: "http://apinet.manenova.com/api/auth/",
